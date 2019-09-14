b7fa23cc5b81a95d91c55ddd8cf7cac9011b1d48
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 12:58:34 -0400

    get profile picture urls

--- graph-api/graph-api.js
+++ graph-api/graph-api.js
@@ -5,12 +5,16 @@ const callGraphApi = async () => {
   const appId = "683297672043582";
   const userAccessToken = "";
 
-  axios
-    .get(
-      `https://graph.facebook.com/117417339643049?fields=friends{picture}&access_token=EAAJtdKMEnD4BAFalkRtsoZCNuyk4NkZCxJ8wEHlW8WrVo1IjPBZAaIuO3daGgLIEsZBc5tD9VMmwfiZBuHbrJduf1EZCSf6GSWo0kr1I090n2ZC25z8hBEezm1a194ZASoaEjf8ItjzDnHVPCPfWbSKAZBekS79aIbNQQELDoBdeRDohqktKGsTDx53EUZAjMTxIZCnhSZBkftodFuGyzTxKHVnr`
+  const response = await new Promise((res, rej) =>
+    res(
+      axios.get(
+        `https://graph.facebook.com/117417339643049?fields=friends{picture.type(large)}&access_token=EAAJtdKMEnD4BAFalkRtsoZCNuyk4NkZCxJ8wEHlW8WrVo1IjPBZAaIuO3daGgLIEsZBc5tD9VMmwfiZBuHbrJduf1EZCSf6GSWo0kr1I090n2ZC25z8hBEezm1a194ZASoaEjf8ItjzDnHVPCPfWbSKAZBekS79aIbNQQELDoBdeRDohqktKGsTDx53EUZAjMTxIZCnhSZBkftodFuGyzTxKHVnr`
+      )
     )
-    .then(res => console.log(res.data.friends.data[0]))
-    .catch(e => console.log(e));
+  );
+  const friendNodes = response.data.friends.data;
+  const imgUrls = friendNodes.map(node => node.picture.data.url);
+  console.log(imgUrls);
 };
 
 module.exports = callGraphApi;
