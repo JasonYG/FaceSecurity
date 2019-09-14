d813d839df3812b73884abb63433edf33ffecb4a
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 12:02:10 -0400

    add test call to graph api

--- graph-api/main.js
+++ graph-api/main.js
@@ -1,10 +1,12 @@
 const axios = require("axios");
+const callGraphApi = require("./graph-api");
 
 const graphApi = async () => {
   const initialResponse = await new Promise((res, rej) =>
     res(axios.get("http://localhost:3000/"))
   );
   console.log(initialResponse.data);
+  callGraphApi();
 };
 
 graphApi();
