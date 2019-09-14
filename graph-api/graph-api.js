18357530e2d3e79d8aaa100d5f71230b2934c5d5
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 13:38:54 -0400

    download images from graph api in base64

--- graph-api/graph-api.js
+++ graph-api/graph-api.js
@@ -8,13 +8,25 @@ const callGraphApi = async () => {
   const response = await new Promise((res, rej) =>
     res(
       axios.get(
-        `https://graph.facebook.com/117417339643049?fields=friends{picture.type(large)}&access_token=EAAJtdKMEnD4BAFalkRtsoZCNuyk4NkZCxJ8wEHlW8WrVo1IjPBZAaIuO3daGgLIEsZBc5tD9VMmwfiZBuHbrJduf1EZCSf6GSWo0kr1I090n2ZC25z8hBEezm1a194ZASoaEjf8ItjzDnHVPCPfWbSKAZBekS79aIbNQQELDoBdeRDohqktKGsTDx53EUZAjMTxIZCnhSZBkftodFuGyzTxKHVnr`
+        `https://graph.facebook.com/117417339643049?fields=friends{picture.type(large)}&access_token=EAAJtdKMEnD4BALr3vL1SCOlj8k4Hi2eZBpb7oqjjfPHsww3obZCaiCuWCZA1ZAsrl1T7tP98JgUjJLRDnsTXpC6tm6Rxq6dRgMH678IEgAGSatZCvlzyZBlk4WIXB5awNbZBiaTWFWu8LpMfRonjWbosOY7hRWEycaBcqcv8KgflEv6ECqqW7wqIxJTfCVhwKQ5UFdmlglY0lN8jEAw6gUi`
       )
     )
   );
   const friendNodes = response.data.friends.data;
   const imgUrls = friendNodes.map(node => node.picture.data.url);
-  console.log(imgUrls);
+
+  const downloadedImgs = await Promise.all(
+    imgUrls.map(
+      url =>
+        new Promise((res, rej) =>
+          res(axios.get(url, { responseType: "arraybuffer" }))
+        )
+    )
+  );
+  const encodedImages = downloadedImgs.map(img =>
+    new Buffer(img.data, "binary").toString("base64")
+  );
+  console.log(encodedImages);
 };
 
 module.exports = callGraphApi;
