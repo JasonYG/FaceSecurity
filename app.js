4ad8156e2f818a0cf153adcfdd273f372a551989
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 01:11:34 -0400

    add boilerplate express code

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => res.send("the server is working"));
+
+app.listen(port, () => console.log(`Listening to port ${port}!`));
