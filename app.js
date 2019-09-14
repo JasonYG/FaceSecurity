af01a7efe1160f4c6d11af9b83d513c50130c739
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 14:29:58 -0400

    add post request field back

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const graphApi = require("./graph-api/main");
+const graphApi = require("./graph-api/main");
 
 app.get("/", (req, res) => res.send({ msg: "the server is working" }));
 
-// app.post("/detect-img", (req, res) => {
-//   res(graphApi());
-// });
+app.post("/detect-img", (req, res) => {
+  res(graphApi());
+});
 
 app.listen(port, () => console.log(`Listening to port ${port}!`));
