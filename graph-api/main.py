6d4516ee364d65994c5be3f59a76f68d6467a530
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 19:24:51 -0400

    refactor api call to function

--- graph-api/main.py
+++ graph-api/main.py
@@ -1,18 +1,19 @@
 import requests
 
-URL = "https://graph.facebook.com/117417339643049?fields=friends{picture.type(large),name}&access_token=EAAJtdKMEnD4BAGlGJZBsIp6a73oA4oZC3fyuKYDBQiPNO0uNyEU2hUhDHzCOeAfbtTJMdYpTWNl0zhcHhiC3qzzFZCZASGuaAJDziMHLoxke2Ipw89t8TJ0CF2HLyfG4dElzno6GfiAMoXShPY0KdVGQPzZBxe1ZBtTlkkHN8925ZBMeSM7GcPG"
 
+def get_profiles():
+    URL = "https://graph.facebook.com/117417339643049?fields=friends{picture.type(large),name}&access_token=EAAJtdKMEnD4BAGlGJZBsIp6a73oA4oZC3fyuKYDBQiPNO0uNyEU2hUhDHzCOeAfbtTJMdYpTWNl0zhcHhiC3qzzFZCZASGuaAJDziMHLoxke2Ipw89t8TJ0CF2HLyfG4dElzno6GfiAMoXShPY0KdVGQPzZBxe1ZBtTlkkHN8925ZBMeSM7GcPG"
 
-response = requests.get(url=URL)
-friends_profile = response.json()['friends']['data']
+    response = requests.get(url=URL)
+    friends_profile = response.json()['friends']['data']
 
-[print(friend) for friend in friends_profile]
+    [print(friend) for friend in friends_profile]
 
-profile_images = {}
-for friend in friends_profile:
-    profile_images[friend['name']] = friend['picture']['data']['url']
+    profile_images = {}
+    for friend in friends_profile:
+        profile_images[friend['name']] = friend['picture']['data']['url']
 
-for name, image_url in profile_images.items():
-    profile_images[name] = requests.get(image_url).content
+    for name, image_url in profile_images.items():
+        profile_images[name] = requests.get(image_url).content
 
-print(profile_images)
+    return profile_images
