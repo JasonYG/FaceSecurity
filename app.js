534b6d4ffd1dbd8d64984363c0a7d00dd725ad54
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 01:22:31 -0400

    fix syntax error with import

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 const port = process.env.port || 3000;
 
-app.get("/", (req, res) => res.send("the server is working"));
+app.get("/api", (req, res) => res.send("the server is working"));
 
 app.listen(port, () => console.log(`Listening to port ${port}!`));
