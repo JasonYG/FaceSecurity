fb1d8d1f285f24f33cc78272796aa6301fa0fa2b
Jason Guo <jasonyzguo@gmail.com>
2019-09-14 15:59:16 -0400

    initial commit for aws-api

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const graphApi = require("./graph-api/main");
 app.get("/", (req, res) => res.send({ msg: "the server is working" }));
 
 app.post("/detect-img", (req, res) => {
+  // Get img from request
+  console.log(req.body);
+  // Check the img with aws collection
+
   res.send({ encodedImages: graphApi() });
 });
 
