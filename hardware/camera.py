65bb898a7fa0fcd2e5652fbfef683eff33026e7b
Jonathan Xu <jonathanxu25@gmail.com>
2019-09-14 12:44:45 -0400

    adding jason's beautiful face

--- hardware/camera.py
+++ hardware/camera.py
@@ -5,15 +5,14 @@ import requests
 camera = PiCamera()
 camera.start_preview()
 
-while(True):
-    print("capturing...")
-    camera.capture('/home/pi/Desktop/image.jpg')
-    print("retrieving file...")
-    files = {'file': open('/home/pi/Desktop/image.jpg', 'rb')}
-    print("posting file...")
-    #r = requests.post('https://2adc0fc4.ngrok.io/upload/1', files=files)
-    #r.text
-    sleep(1)
+print("capturing...")
+camera.capture('/home/pi/Desktop/image.jpg')
+print("retrieving file...")
+files = {'file': open('/home/pi/Desktop/image.jpg', 'rb')}
+print("posting file...")
+#r = requests.post('https://2adc0fc4.ngrok.io/upload/1', files=files)
+#r.text
+sleep(1)
 
 
 camera.stop_preview()
